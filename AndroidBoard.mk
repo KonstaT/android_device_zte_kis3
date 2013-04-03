32ad9b0823407ec21fda79f6d8d025e95a539667
Sameer Thalappil <sameert@codeaurora.org>
2013-04-03 14:45:08 -0700

    wlan: add WLAN config files.
    Add config files to enable WiFi on msm8610 target.
    
    Change-Id: Ic2af169f0c3d737e64dd49c66954bec837a9a27a

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -75,6 +75,58 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+ifeq ($(strip $(BOARD_HAS_ATH_WLAN_AR6004)),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := wpa_supplicant_ath6kl.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+include $(BUILD_PREBUILT)
+endif
+
+ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := wpa_supplicant_wcn.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := hostapd_default.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
+LOCAL_SRC_FILES    := hostapd.conf
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := hostapd.accept
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
+LOCAL_SRC_FILES    := hostapd.accept
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := hostapd.deny
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
+LOCAL_SRC_FILES    := hostapd.deny
+include $(BUILD_PREBUILT)
+
+#Create symbolic links
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
+        ln -sf /persist/WCNSS_qcom_wlan_nv.bin \
+        $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
+        ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
+        $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+
+endif
+
 #----------------------------------------------------------------------
 # extra images
 #----------------------------------------------------------------------
