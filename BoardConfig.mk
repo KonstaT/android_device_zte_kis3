5c5b0d7bc4bd617ffda2202cc0adf6c81492be6e
Dhivya Subramanian <dthiru@codeaurora.org>
2013-06-21 12:25:36 -0700

    Set TARGET_CPU_VARIANT to krait for 8610
    TARGET_CPU_VARIANT must be set to a value.
    
    Change-Id: I5719db55fd89a1da16790b5dac0bfb616b59db66

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,7 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_CPU_ABI  := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := krait
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
