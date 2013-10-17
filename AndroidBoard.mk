1969f50aa67ef84fd2696a0e1c8d8cc626793088
Hardik Kantilal Patel <hkpatel@codeaurora.org>
2013-10-17 11:12:52 +0530

    msm8610: Remove wpa_supplicant.conf declaration from AndroidBoard.mk
    Information on wpa_supplicant.conf is specified in Android.mk in the path
    hardware/qcom/wlan/qcwcn/config/. Hence remove the same from
    AndroidBoard.mk
    
    Change-Id: I4c03c18c09778213e3883284c42660dff1622884

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -104,14 +104,6 @@ endif
 
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
-LOCAL_MODULE       := wpa_supplicant.conf
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := wpa_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
