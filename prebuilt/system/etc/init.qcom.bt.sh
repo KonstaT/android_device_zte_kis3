8338c3b156156d6069d767efa5487aca3cfb2196
Srinu Jella <sjella@codeaurora.org>
2013-11-25 15:22:29 +0530

    bluetooth: Add DUN support for APQ8084 target
    This change will enable support for DUN on APQ8084
    platform.
    
    Handled the DUN feature enablement based on target
    under modem section since DUN feature is dependent
    on modem support.
    
    Change-Id: If8600ee82a271f33b8e7df0ffd7eb56622fa02eb

--- prebuilt/system/etc/init.qcom.bt.sh
+++ prebuilt/system/etc/init.qcom.bt.sh
@@ -103,7 +103,15 @@ config_bt ()
         setprop ro.qualcomm.bluetooth.map true
         setprop ro.qualcomm.bluetooth.nap true
         setprop ro.bluetooth.sap true
-        setprop ro.bluetooth.dun false
+        case $target in
+          "apq8084")
+              setprop ro.bluetooth.dun true
+              logi "Enabling BT-DUN for APQ8084"
+              ;;
+          *)
+              setprop ro.bluetooth.dun false
+              ;;
+        esac
         ;;
     # ZTE_MODIFY cuiningning for bluetooth SIG certification disable MAP,SAP,DUN 20140409
     "msm")
@@ -162,7 +170,6 @@ config_bt ()
        elif [ "$btsoc" = "rome" ]
        then
            setprop ro.bluetooth.hfp.ver 1.6
-           setprop ro.bluetooth.dun false
        fi
        ;;
     *)
