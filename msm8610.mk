6b83ba4acf178ebab2bf4853392c75937c00b728
AUDITYA BHATTARAM <audityab@codeaurora.org>
2013-12-11 13:03:17 +0530

    Fix User build Issues.
    Remove qcmediaplayer & qcom.fmradio from PRODUCT_BOOT_JARS.
    Substitute space between PRODUCT_JARS with ':'
    
    Change-Id: Icd4bd1ab7f71986132ef324dc2d18342ef105d6a

--- msm8610.mk
+++ msm8610.mk
@@ -22,7 +22,7 @@ $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 PRODUCT_NAME := msm8610
 PRODUCT_DEVICE := msm8610
 
-PRODUCT_BOOT_JARS += qcmediaplayer:oem-services:qcom.fmradio:org.codeaurora.Performance
+PRODUCT_BOOT_JARS += oem-services:org.codeaurora.Performance
 
 # Audio configuration file
 PRODUCT_COPY_FILES += \
