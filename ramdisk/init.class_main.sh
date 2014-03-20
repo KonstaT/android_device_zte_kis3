18b29841a7e91fedd96a4240c581bea5d7d22c49
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2014-03-20 16:46:56 +0530

    ramdisk: Remove restart of ril-daemon
    Issue: Default rild was not launching some times
    Fix: This is a timing issue, fixed this by removing
    stop and start of default ril-daemon in init.class_main.sh
    
    Change-Id: I736994ddeaade729f7cb5ffd6f418a7c7c2c977e
    CRs-Fixed: 635725

--- ramdisk/init.class_main.sh
+++ ramdisk/init.class_main.sh
@@ -60,13 +60,8 @@ case "$baseband" in
     multisim=`getprop persist.radio.multisim.config`
 
     if [ "$multisim" = "dsds" ] || [ "$multisim" = "dsda" ]; then
-        # ZTE_MODIFY maxiaoqiang for ril-daemon not starting
-        #stop ril-daemon
-        #start ril-daemon
         start ril-daemon1
     elif [ "$multisim" = "tsts" ]; then
-        stop ril-daemon
-        start ril-daemon
         start ril-daemon1
         start ril-daemon2
     fi
