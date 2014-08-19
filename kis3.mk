3ae6d7d957d4851ec73e5d747d83b3364802f9ad
Konsta <konsta09@gmail.com>
2014-08-19 13:46:27 +0300

    remove more unused stuff

--- kis3.mk
+++ kis3.mk
@@ -25,6 +25,7 @@ PRODUCT_COPY_FILES += \
     device/zte/kis3/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     device/zte/kis3/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Audio
 PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
@@ -40,31 +41,11 @@ PRODUCT_COPY_FILES += \
     device/zte/kis3/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     device/zte/kis3/WCNSS_qcom_wlan_nv.bin:persist/WCNSS_qcom_wlan_nv.bin
 
-#ANT stack
-PRODUCT_PACKAGES += \
-    AntHalService \
-    libantradio \
-    antradio_app
-
 PRODUCT_PACKAGES += \
     wpa_supplicant_overlay.conf \
     p2p_supplicant_overlay.conf
 
 PRODUCT_PACKAGES += wcnss_service
-
-# Sensors feature definition file/s
-PRODUCT_COPY_FILES += \
-   frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
-
-# Enable strict operation
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.strict_op_enable=false
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.whitelist=/system/etc/whitelist_appops.xml
-
-PRODUCT_COPY_FILES += \
-    device/zte/kis3/whitelist_appops.xml:system/etc/whitelist_appops.xml
 
 # Gecko low-memory killer setting overrides
 #
