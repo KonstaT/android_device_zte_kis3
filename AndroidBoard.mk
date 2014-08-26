0d4b033266e06c5f6bfcac5670f5e1e538254011
Konsta <konsta09@gmail.com>
2014-08-26 16:03:58 +0300

    wifi: import zte modifications

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -11,8 +11,8 @@ $(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
 #----------------------------------------------------------------------
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
         rm $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
-        ln -sf /persist/WCNSS_qcom_wlan_factory_nv.bin \
-        $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_factory_nv.bin)
+        ln -sf /persist/WCNSS_qcom_wlan_nv.bin \
+        $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin)
 
 #----------------------------------------------------------------------
 # extra images
