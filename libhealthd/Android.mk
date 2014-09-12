f2bfa5d8f884848dfc0ead0d132009fe43ec2c15
Fenglin Wu <fenglinw@codeaurora.org>
2014-09-12 11:07:28 +0800

    libhealthd: Draw battery capacity in charging animation
    Draw a text to show the battery capacity value in charging animation.
    
    Change-Id: I0117fb0aee69181ce0d608ba4b1644fa42cd1a0d

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -3,6 +3,8 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := healthd_board_default.cpp
 LOCAL_MODULE := libhealthd.qcom
-LOCAL_C_INCLUDES := system/core/healthd
+LOCAL_C_INCLUDES := \
+    system/core/healthd \
+    bootable/recovery
 
 include $(BUILD_STATIC_LIBRARY)
--- libhealthd/healthd_board_default.cpp
+++ libhealthd/healthd_board_default.cpp
@@ -15,6 +15,7 @@
  */
 
 #include <healthd.h>
+#include "minui/minui.h"
 
 void healthd_board_init(struct healthd_config*)
 {
@@ -27,9 +28,22 @@ int healthd_board_battery_update(struct android::BatteryProperties*)
     return 1;
 }
 
-void healthd_board_mode_charger_draw_battery(struct android::BatteryProperties*)
+#define STR_LEN 8
+void healthd_board_mode_charger_draw_battery(struct android::BatteryProperties *batt_prop)
 {
+    char cap_str[STR_LEN];
+    int x, y;
+    int str_len_px;
+    static int char_height = -1, char_width = -1;
 
+    if (char_height == -1 && char_width == -1)
+        gr_font_size(&char_width, &char_height);
+    snprintf(cap_str, (STR_LEN - 1), "%d%%", batt_prop->batteryLevel);
+    str_len_px = gr_measure(cap_str);
+    x = (gr_fb_width() - str_len_px) / 2;
+    y = (gr_fb_height() + char_height) / 2;
+    gr_color(0xa4, 0xc6, 0x39, 255);
+    gr_text(x, y, cap_str, 0);
 }
 
 void healthd_board_mode_charger_battery_update(struct android::BatteryProperties*)
