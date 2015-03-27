ba72fec2f4b54a69273e54bfeb54a130aeeea980
Konsta <konsta09@gmail.com>
2015-03-27 16:56:42 +0200

    fm: remove unnecessary script and files
    * Set the no firmware download flag instead.
    
    Change-Id: Id6fef5443af63840678483187fe108df1bfa35ee

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,6 +70,9 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 
+# FM radio
+TARGET_QCOM_NO_FM_FIRMWARE := true
+
 # Graphics
 BOARD_EGL_CFG := device/zte/kis3/prebuilt/system/lib/egl/egl.cfg
 USE_OPENGL_RENDERER := true
