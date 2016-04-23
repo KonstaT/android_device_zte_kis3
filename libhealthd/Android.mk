835798e3746863f0b5616cc70a5e8ac1905d626b
Konsta <konsta09@gmail.com>
2016-04-23 10:56:51 +0300

    healthd: rename
    * https://github.com/CyanogenMod/android_system_core/commit/4ca66c4d62bd1867520ee42c7077503ac8582c10
    * https://github.com/CyanogenMod/android_system_core/commit/05a3648f9529cffecb24f939768868380de307d9
    
    Change-Id: I5648be6702c15a7010681108b7012c41146ac4a0

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := healthd_board_default.cpp
-LOCAL_MODULE := libhealthd.qcom
+LOCAL_SRC_FILES := healthd_board_kis3.cpp
+LOCAL_MODULE := libhealthd.kis3
 LOCAL_C_INCLUDES := \
     system/core/healthd \
     bootable/recovery
